6cc71e83d742fa7c8e90b1a6716d17a14743976c
Andrii Bodnar <andrii.bodnar@crowdin.com>
2025-09-30 15:22:23 +0300

    chore: remove deployment banner

--- src/components/layout/global-header.tsx
+++ src/components/layout/global-header.tsx
@@ -3,7 +3,6 @@ import { AuthButton } from '../auth/auth-button';
 import { ThemeToggle } from '../theme-toggle';
 import { motion } from 'framer-motion';
 import { useAuth } from '@/contexts/auth-context';
-import { GithubIcon } from 'lucide-react';
 import { CloudflareLogo } from '../icons/logos';
 
 export function GlobalHeader() {
@@ -54,17 +53,6 @@ export function GlobalHeader() {
 						transition={{ delay: 0.2 }}
 						className="flex items-center gap-2"
 					>
-						<div className="gap-6 flex flex-col justify-between border px-4 bg-bg-4 dark:bg-bg-2 rounded-md py-2 border-accent/50 dark:border-accent/50 !border-t-transparent rounded-t-none mr-12">
-							<div className="flex w-full gap-2 items-center">
-								<div className='text-text-primary/80 mr-4 text-2xl font-medium'>Deploy your own vibe-coding platform</div>
-								<div className="flex font-semibold gap-2 items-center bg-accent dark:bg-accent text-white rounded px-2 hover:opacity-80 cursor-pointer" onClick={() => window.open("https://deploy.workers.cloudflare.com/?url=https://github.com/cloudflare/vibesdk", "_blank")}>
-									Deploy <CloudflareLogo className='w-5 h-5' color1='#fff'  />
-								</div>
-								<div className="flex font-semibold items-center bg-text-primary text-bg-4 rounded gap-1 px-2 hover:opacity-80 cursor-pointer" onClick={() => window.open("https://github.com/cloudflare/vibesdk", "_blank")} >
-									Fork <GithubIcon className="size-4" />
-								</div>
-							</div>
-						</div>
                         {/* Disable cost display for now */}
 						{/* {user && (
 							<CostDisplay
